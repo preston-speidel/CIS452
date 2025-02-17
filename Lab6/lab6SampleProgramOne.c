5d9a29c86f7f92d2ae7f6c9ffcacd17bc649e8a8
preston-speidel <125756232+preston-speidel@users.noreply.github.com>
2025-02-17 14:25:24 -0500

    sample program one

--- Lab6/lab6SampleProgramOne.c
+++ Lab6/lab6SampleProgramOne.c
@@ -6,68 +6,115 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <sys/sem.h>
 
 #define SIZE 16
+
+// Functions to perform semaphore operations (wait and signal)
+void sem_wait(int sem_id) {
+    struct sembuf sb = {0, -1, 0};
+    semop(sem_id, &sb, 1);
+}
+
+void sem_signal(int sem_id) {
+    struct sembuf sb = {0, 1, 0}; 
+    semop(sem_id, &sb, 1);
+}
 
 int main(int argc, char *argv[])
 {
     int status;
     long int i, loop, temp, *sharedMemoryPointer;
     int sharedMemoryID;
+    // Semaphore ID
+    int sem_id;  
     pid_t pid;
 
     loop = atoi(argv[1]);
 
-    sharedMemoryID = shmget(IPC_PRIVATE, SIZE, IPC_CREAT|S_IRUSR|S_IWUSR);
+    // Create shared memory
+    sharedMemoryID = shmget(IPC_PRIVATE, SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
     if(sharedMemoryID < 0) {
-        perror ("Unable to obtain shared memory\n");
-        exit (1);
+        perror("Unable to obtain shared memory");
+        exit(1);
     }
 
+    // Attach shared memory
     sharedMemoryPointer = shmat(sharedMemoryID, 0, 0);
     if(sharedMemoryPointer == (void*) -1) {
-        perror ("Unable to attach\n");
-        exit (1);
+        perror("Unable to attach");
+        exit(1);
     }
 
     sharedMemoryPointer[0] = 0;
     sharedMemoryPointer[1] = 1;
+
+    // Create the semaphore
+    sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    if(sem_id < 0) {
+        perror("Unable to create semaphore");
+        exit(1);
+    }
+
+    // Initialize the semaphore to 1 (available)
+    if(semctl(sem_id, 0, SETVAL, 1) < 0) {
+        perror("Unable to initialize semaphore");
+        exit(1);
+    }
 
     pid = fork();
     if(pid < 0){
         printf("Fork failed\n");
     }
 
-    if(pid == 0) { // Child
-        for(i=0; i<loop; i++) {
-            // swap the contents of sharedMemoryPointer[0] and sharedMemoryPointer[1]
+    // Child Process
+    if(pid == 0) { 
+        for(i = 0; i < loop; i++) {
+            // Lock before swapping
+            sem_wait(sem_id);  
             temp = sharedMemoryPointer[0];
             sharedMemoryPointer[0] = sharedMemoryPointer[1];
             sharedMemoryPointer[1] = temp;
+            // Unlock after swapping
+            sem_signal(sem_id);  
         }
         if(shmdt(sharedMemoryPointer) < 0) {
-            perror ("Unable to detach\n");
-            exit (1);
+            perror("Unable to detach");
+            exit(1);
         }
         exit(0);
-    }
-    else
-        for(i=0; i<loop; i++) {
-            // swap the contents of sharedMemoryPointer[1] and sharedMemoryPointer[0]
+    } 
+    // Parent Process
+    else { 
+        for(i = 0; i < loop; i++) {
+            // Lock before swapping
+            sem_wait(sem_id);  
             temp = sharedMemoryPointer[1];
             sharedMemoryPointer[1] = sharedMemoryPointer[0];
             sharedMemoryPointer[0] = temp;
+            // Unlock after swapping
+            sem_signal(sem_id);  
         }
+    }
 
     wait(&status);
     printf("Values: %li\t%li\n", sharedMemoryPointer[0], sharedMemoryPointer[1]);
 
+    // Detach shared memory
     if(shmdt(sharedMemoryPointer) < 0) {
-        perror ("Unable to detach\n");
-        exit (1);
+        perror("Unable to detach");
+        exit(1);
     }
+
+    // Remove shared memory
     if(shmctl(sharedMemoryID, IPC_RMID, 0) < 0) {
-        perror ("Unable to deallocate\n");
+        perror("Unable to deallocate");
+        exit(1);
+    }
+
+    // Remove the semaphore
+    if(semctl(sem_id, 0, IPC_RMID) < 0) {
+        perror("Unable to remove semaphore");
         exit(1);
     }
 
